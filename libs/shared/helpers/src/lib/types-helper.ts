e36f6ffb4b02a00ce2eb4632e2b4b348e4c3d891
Nam Nguyen <hoangnamnguyen191@gmail.com>
2023-09-02 19:13:44 -0400

    feat: improve strong route typing

--- libs/shared/helpers/src/lib/types-helper.ts
+++ libs/shared/helpers/src/lib/types-helper.ts
@@ -4,10 +4,13 @@ declare const brand: unique symbol;
 
 export type Brand<T, TBrand> = T & { [brand]: TBrand };
 
-export type ExtractParamsFromRoute<TRoute extends string> = {
+export type ExtractParamsFromRoute<
+  TRoute extends string,
+  TParamVal extends string
+> = {
   [K in S.Split<TRoute, '/'>[number] as K extends `:${infer TParam}`
     ? TParam
-    : never]: string;
+    : never]: TParamVal;
 };
 
 type RouteFromParamsMap<
@@ -27,7 +30,10 @@ export type RoutesFromPath<
   TRoute extends string
 > = {
   [k in keyof TPaths]: TPaths[k] extends `${string}:${string}`
-    ? <TParams extends ExtractParamsFromRoute<TPaths[k]>>(
+    ? <
+        TParamsValue extends string,
+        TParams extends ExtractParamsFromRoute<TPaths[k], TParamsValue>
+      >(
         params: TParams
       ) => RouteFromParamsMap<TPaths[k], TParams>
     : `/${TRoute}/${TPaths[k]}`;
