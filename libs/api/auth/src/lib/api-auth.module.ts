184b4f510ff0f58fa0bda7eeb8978b0abf887a4b
Nam Nguyen <hoangnamnguyen191@gmail.com>
2023-10-09 11:35:15 -0400

    feat: fuk it and use nest config module

--- libs/api/auth/src/lib/api-auth.module.ts
+++ libs/api/auth/src/lib/api-auth.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
+import { ApiUsersModule } from '@v-notes/api/users';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ApiUsersModule } from '@v-notes/api/users';
 
 @Module({
-  imports: [
-    JwtModule.register({
-      global: true,
-      secret: process.env['JWT_SECRETS'],
-      signOptions: { expiresIn: '1d' },
-    }),
-    ApiUsersModule,
-  ],
+  imports: [ApiUsersModule],
   controllers: [AuthController],
   providers: [AuthService],
-  exports: [AuthService],
+  exports: [AuthService]
 })
 export class ApiAuthModule {}
--- libs/api/core/src/lib/api-core.module.ts
+++ libs/api/core/src/lib/api-core.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ApiAuthModule } from '@v-notes/api/auth';
 import { ApiBoardModule } from '@v-notes/api/board';
@@ -6,9 +8,15 @@ import { ApiUsersModule } from '@v-notes/api/users';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env['DATABASE_USER']}:${process.env['DATABASE_PASSWORD']}@cluster0.kurrwda.mongodb.net/?retryWrites=true&w=majority`
     ),
+    JwtModule.register({
+      global: true,
+      secret: process.env['JWT_SECRETS'],
+      signOptions: { expiresIn: '1d' }
+    }),
     ApiUsersModule,
     ApiAuthModule,
     ApiBoardModule
